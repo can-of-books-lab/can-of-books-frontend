b53b06d03478cac04ede0ae4c2abe3a72bd4b4e4
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-03-01 17:16:04 -0800

    finished cart 2 lab 12

--- src/modules/Bestbooks.js
+++ src/modules/Bestbooks.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 import axios from "axios";
+import AddBookButton from "./AddBookButton";
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class BestBooks extends React.Component {
       bookData: [],
     };
   }
-    /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
   handleGetBooks = async () => {
     try {
@@ -33,17 +34,33 @@ class BestBooks extends React.Component {
   componentDidMount() {
     this.handleGetBooks();
   }
-    /* TODO: render user's books in a Carousel */
+
+  handlePostBook = async (newBook) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`
+      let createdBook = await axios.post(url, newBook) 
+        console.log(createdBook.data);
+        this.setState({bookData: [...this.state.bookData, createdBook.data]})
+    } catch (error) {
+      console.log('Error in the post request.', error.response.data)
+      alert('Sorry, there was an error.  Your book wasn\'t added')
+    }
+  }
+
+  /* TODO: render user's books in a Carousel */
   render() {
     console.log(this.state.bookData);
     return (
-      <>
+     <>
         {this.state.bookData.length ? (
           <Row xs={1} sm={2} md={3} lg={4} className="mt-5">
-            {this.state.bookData.map((book, index) => (
-              <Col key={index}>
+              <Col>
+                <AddBookButton
+                  handlePostBook={this.handlePostBook}
+                  />
                 <Carousel fade>
-                  <Carousel.Item>
+                  {this.state.bookData.map((book, index) => (
+                  <Carousel.Item key={index}>
                     <img
                       className="d-block w-100"
                       src="https://place-hold.it/3200x1800"
@@ -55,10 +72,9 @@ class BestBooks extends React.Component {
                       <p>{book.status}</p>
                       <p>{book.email}</p>
                     </Carousel.Caption>
-                  </Carousel.Item>
+                  </Carousel.Item>))}
                 </Carousel>
               </Col>
-            ))}
           </Row>
         ) : (
           <h3>No Books Found :(</h3>
